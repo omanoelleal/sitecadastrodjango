f16036da1fb1d2d61cf72497115c6716664ad6bf
Manoel Leal <manoel.leal17@gmail.com>
2023-03-14 19:58:41 +0000

    change html

--- projeto_cad_users/app_cad_users/views.py
+++ projeto_cad_users/app_cad_users/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Usuario
 
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
-    pass
+    #Salvar os dados do novo usuário
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    return render(request, 'usuarios/usuarios.html', usuarios)
